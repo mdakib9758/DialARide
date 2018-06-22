dd9c12a9c3b10f40d4a4350ec30d56f68483024c
mdakib9758 <mdakib51@gmail.com>
2018-06-22 16:45:36 +0530

    Input completed
    all input has completed now oprations classes left

--- Car.java
+++ Car.java
@@ -1,6 +1,53 @@
 package com.hackercode.utilities;
 
 public class Car {
+	private int carTime;
+	private int capacity;
+	private int location;
+	private int revaneu;
+	private int[]passengerloacation;
+	public Car(){
+		passengerloacation=new int[5];
+		capacity=5;
+	}
+	public int getCarTime() {
+		return carTime;
+	}
+	public void passengerLocationReset(){
+		for(int i=0;i<10;i++){
+			passengerloacation[i]=0;
+		}
 		
+	}
+	public void setCarTime(int carTime) {
+		this.carTime = carTime;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	public int getLocation() {
+		return location;
+	}
+	public int getRevaneu() {
+		return revaneu;
+	}
+	public int[] getPassengerloacation() {
+		return passengerloacation;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public void setLocation(int location) {
+		this.location = location;
+		//System.out.println(location);
+	}
+	public void setRevaneu(int revaneu) {
+		this.revaneu = revaneu;
+	}
+	public void setPassengerloacation(int[] passengerloacation) {
+		this.passengerloacation = passengerloacation;
+	}
+	
 	
 }
--- Request.java
+++ Request.java
@@ -1,5 +1,38 @@
 package com.hackercode.utilities;
 
 public class Request {
-		
+	int src,dest,srtTime,endTime;
+
+	public int getSrc() {
+		return src;
+	}
+
+	public int getDest() {
+		return dest;
+	}
+
+	public int getSrtTime() {
+		return srtTime;
+	}
+
+	public int getEndTime() {
+		return endTime;
+	}
+
+	public void setSrc(int src) {
+		this.src = src;
+	}
+
+	public void setDest(int dest) {
+		this.dest = dest;
+	}
+
+	public void setSrtTime(int srtTime) {
+		this.srtTime = srtTime;
+	}
+
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+	
 }
