ba5ba003d673fd0339357bf01d22349740913cd3
mdakib9758 <mdakib51@gmail.com>
2018-06-25 22:23:52 +0530

    scheduling for one car
    still need to form a code for n car

--- OutputRevaneu.java
+++ OutputRevaneu.java
@@ -1,4 +1,8 @@
 package com.hackercode.utilities;
 
 public class OutputRevaneu {
+	public void writeOutput(Car car[]){
+			System.out.println(" Revaneu="+car[0].getRvaneu());
+		
 	}
+}
--- Scheduling.java
+++ Scheduling.java
@@ -3,5 +3,46 @@ package com.hackercode.utilities;
 import com.hackercode.constants.Constants;
 
 public class Scheduling {
-	
+	public void schedule(Car car[],Request r[],int loc[][]){
+		
+				if(car[0].getLocation()!=r[i].getSrc()){
+				int srtDist=loc[((car[0].getLocation())-1)][((r[i].getSrc())-1)];
+				car[0].setLocation(r[i].getSrc());
+				car[0].setCarTime(2*srtDist);
+			}
+			//wait car
+			if(car[0].getCarTime()<r[i].getSrtTime()){
+				car[0].setCarTime(r[i].srtTime);
+			}
+			//scheduling a request
+			if(car[0].getCapacity()!=0 && (car[0].getCarTime()>=r[i].getSrtTime()) && car[0].getCarTime()<=r[i].endTime){	
+				car[0].setRevaneu(car[0].getRevaneu()+(loc[(car[0].getLocation())-1][(r[i].getDest())-1]));
+				car[0].setCapacity(car[0].getCapacity()-1);
+				int []indeces=car[0].getPassengerloacation();
+				System.out.println(Constants.requestServed+r[i].getSrc()+" "+r[i].getDest()+" "+r[i].getSrtTime() +" "+r[i].getEndTime());
+				for(int j=0;j<5;j++){
+					if(indeces[j]==0){
+						indeces[j]=r[i].getDest();
+						break;
+					}
+				}
+				car[0].setPassengerloacation(indeces);
+			}
+			//drop passenger respective locations
+			if(car[0].getCapacity()==5){
+				int []dropLocations=car[0].getPassengerloacation();
+				for(int j=0;j<dropLocations.length;j++){
+					if(car[0].getLocation()!=dropLocations[i]){
+						int srtDist=loc[(car[0].getLocation())-1][(dropLocations[i])-1];
+						car[0].setLocation(dropLocations[i]);
+						car[0].setCarTime(car[0].getCarTime()+(2*srtDist));
+					}
+					car[0].setCapacity(car[0].getCapacity()-1);
+					car[0].passengerLocationReset();
+				}
+			}
+		}
+		
+	}
+
 }
